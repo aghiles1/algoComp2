9e19acc816a746fdf6d9a6b684353c6068cb3d71
aghiles <aghiles.dziri@etu.unice.fr>
2018-01-03 17:37:19 +0100

    first commit

---
+++ src/main/java/Algo.java
@@ -0,0 +1,12 @@
+public class Algo {
+
+    /**
+     * exécution des 5 algorithmes sur les exemples fournis (exemple100.txt,
+     *exemple500.txt, exemple1000.txt) et sur un exemple que vous donnez, dans un fichier ascii
+     *nommé "monexemple.txt".
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Stat.java
@@ -0,0 +1,10 @@
+public class Stat {
+    /**
+     * effectue une série de simulations sur des exemples générés de manière aléatoire (le
+     * nombre et la manière de générer seront fourni par une phase de dialogue input/output)
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
