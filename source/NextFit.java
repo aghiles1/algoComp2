eea0b890f962354e69dddd9a145efd2c7da06a9a
aghiles <aghiles.dziri@etu.unice.fr>
2018-01-04 23:39:15 +0100

    b

--- source/NextFit.java
+++ source/NextFit.java
@@ -16,16 +16,16 @@ public class NextFit extends AbstractBP
     @Override
     public void run() throws Exception {
         bins = new ArrayList<>();
-        bins.add(binSize);
+        bins.add(0);
         for (int i=0;i < itemsSizes.size(); i++){
             if (binSize < itemsSizes.get(i)){
                 throw new ItemsSizeException();
             }
-            if (bins.get(bins.size()-1) > itemsSizes.get(i)){
-                bins.set(bins.size()-1,bins.get(bins.size()-1)-itemsSizes.get(i));
+            if (bins.get(bins.size()-1) + itemsSizes.get(i) <= binSize){
+                bins.set(bins.size()-1,bins.get(bins.size()-1)+itemsSizes.get(i));
             }else{
-                bins.add(binSize);
-                bins.set(bins.size()-1,bins.get(bins.size()-1)-itemsSizes.get(i));
+                bins.add(0);
+                bins.set(bins.size()-1,bins.get(bins.size()-1)+itemsSizes.get(i));
             }
 
         }
