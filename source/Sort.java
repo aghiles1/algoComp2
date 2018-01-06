4c8452022cf8c8cac6515697919dd948354d02f9
romain <romain.sumerot@etu.unice.fr>
2018-01-06 14:57:32 +0100

    Sort fait

--- source/Sort.java
+++ source/Sort.java
@@ -10,16 +10,65 @@ public class Sort
         this.nodes = nodes;
     }
 
+    private void swap(List<Node> list, int i, int j)
+    {
+        Node nodeI = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, nodeI);
+    }
+
     public List<Node> getByTime()
     {
-        List<Node> result = new ArrayList<>();
+        List<Node> result = new ArrayList<>(nodes);
+
+        if(!result.isEmpty())
+        {
+            for(int i = 0; i < result.size() - 1; i++)
+            {
+                int min = i;
+
+                for(int j = i + 1; j < result.size(); j++)
+                {
+                    if(result.get(j).time < result.get(min).time)
+                    {
+                        min = j;
+                    }
+                }
+
+                if(min != i)
+                {
+                    swap(result, i, min);
+                }
+            }
+        }
 
         return result;
     }
 
     public List<Node> getByEffectiveness()
     {
-        List<Node> result = new ArrayList<>();
+        List<Node> result = new ArrayList<>(nodes);
+
+        if(!result.isEmpty())
+        {
+            for(int i = 0; i < result.size() - 1; i++)
+            {
+                int min = i;
+
+                for(int j = i + 1; j < result.size(); j++)
+                {
+                    if(result.get(j).numberOfBins < result.get(min).numberOfBins)
+                    {
+                        min = j;
+                    }
+                }
+
+                if(min != i)
+                {
+                    swap(result, i, min);
+                }
+            }
+        }
 
         return result;
     }
