0e36a770411191c42c1d737b75ca65a5d3a27918
Elie Baroghel <elie.baroghel@etu.unice.fr>
2018-01-07 22:02:06 +0100

    nettoyage

--- source/FirstFit.java
+++ source/FirstFit.java
@@ -8,22 +8,6 @@ public class FirstFit extends AbstractBP
         super(binSize, items);
     }
 
-    private Bin getFirstBin(Item item) throws ItemsSizeException
-    {
-        for(int i = 0; i < bins.size(); i++)
-        {
-            if(bins.get(i).canAddItem(item))
-            {
-                return bins.get(i);
-            }
-        }
-
-        Bin bin = new Bin(binSize);
-        bins.add(bin);
-
-        return bin;
-    }
-
     private Bin avlTreeFirstFit(AVLTree3<Bin> avlTree, Item item){
         Bin found = null;
 
@@ -49,26 +33,19 @@ public class FirstFit extends AbstractBP
         bins = new ArrayList<>();
         AVLTree3<Bin> tree = new AVLTree3<>();
 
-        for(Item item : items){
+        for(Item item : items) {
             Bin b = avlTreeFirstFit(tree, item);
-            if(b != null){
+            if (b != null) {
                 tree.delete(b);
                 b.addItem(item);
                 tree.insert(b);
-            }else {
+            } else {
                 b = new Bin(binSize);
                 b.addItem(item);
                 bins.add(b);
                 tree.insert(b);
             }
         }
-/*
-        for(int i = 0; i < items.size(); i++)
-        {
-            Item item = items.get(i);
-            Bin firstBin = getFirstBin(item);
-            firstBin.addItem(item);
-        }*/
 
 
         time = (System.nanoTime() - debut)/NSTOMS;
--- source/WorstFit.java
+++ source/WorstFit.java
@@ -8,36 +8,12 @@ public class WorstFit extends AbstractBP
         super(binSize, items);
     }
 
-    private Bin getWorstBin(Item item) throws ItemsSizeException
-    {
-        int worst = -1;
-        Bin b = null;
-        for(Bin bibi : bins){
-            if(binSize - (bibi.getCapacity() + item.getSize()) > worst){
-                worst = binSize - (bibi.getCapacity() + item.getSize());
-                b = bibi;
-            }
-        }
-
-        if(b == null){
-            b = new Bin(binSize);
-            bins.add(b);
-        }
-
-        return b;
-    }
-
     @Override
     public void run() throws Exception
     {
         reset(binSize, items);
 
         long debut = System.nanoTime();
-        /*
-        for(Item item : items){
-            Bin worstBin = getWorstBin(item);
-            worstBin.addItem(item);
-        }*/
 
         bins = new ArrayList<>();
 
