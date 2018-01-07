f57f88666ab0f52402d538a63a45370100d2d2b7
romain <romain.sumerot@etu.unice.fr>
2018-01-08 00:09:02 +0100

    run all algos in Algo.java

--- source/Algo.java
+++ source/Algo.java
@@ -8,11 +8,10 @@ import java.util.List;
 public class Algo
 {
 
-    /**
+    /*
      * exécution des 5 algorithmes sur les exemples fournis (exemple100.txt,
      *exemple500.txt, exemple1000.txt) et sur un exemple que vous donnez, dans un fichier ascii
      *nommé "monexemple.txt".
-     * @param args
      */
     public static void main(String[] args) throws Exception
     {
